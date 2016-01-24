978645cf4742bec5bf51e9969aaffaffbfb9a932
BooMamoo <yanakorn_eve@hotmail.com>
2016-01-24 10:06:36 +0000

    Edit 'ListDevicePage'

--- public/js/ListController.js
+++ public/js/ListController.js
@@ -1,3 +1,7 @@
-app.controller('ListController', function($scope, datas){
+app.controller('ListController', function($scope, $location, datas){
 	$scope.devices = datas.data.devices;
+
+	$scope.getInfo = function(device_id) {
+        $location.path('/device/' + device_id + '/info');
+    }
 });
