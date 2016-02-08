4c19c1e603052a00b1600e46e35d376298e6f03f
BooMamoo <yanakorn_eve@hotmail.com>
2016-02-08 01:19:04 +0000

    Create 'ControllerDirectory' and move file to the directory

--- public/js/ListController.js
+++
@@ -1,7 +0,0 @@
-app.controller('ListController', function($scope, $location, data){
-	$scope.devices = data.data.devices;
-
-	$scope.getInfo = function(device_id) {
-        $location.path('/device/' + device_id + "/info");
-    }
-});
