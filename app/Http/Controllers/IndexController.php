25dad493577942d43941c8d41e982a2125c8ef2f
BooMamoo <yanakorn_eve@hotmail.com>
2016-02-09 14:14:03 +0000

    Edit 'ConvertFunction' and change banner background

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -29,7 +29,6 @@ class Indexcontroller extends Controller
     	$type_id = Type::select('id')->where('type', '=', $type)->get();
     	$mapping = Mapping::where('device_id', '=', $device_id[0]->id)->where('type_id', '=', $type_id[0]->id)->get();
     	$value = $this->convert($mapping[0]->formula, $value);
-    	// $test = $this->convert("(x*(9/50))+32", 789); //"(x*(9/50))+32"
 
     	$information = new Information;
     	$information->mapping_id = $mapping[0]->id;
@@ -42,117 +41,9 @@ class Indexcontroller extends Controller
 
     public function convert($formula, $value)
     {
-    	$tmp = [];
-    	$convert = 0;
-    	$count = 0;
-    	$symbols = ['+', '-', '*', '/', '^'];
+        $substitute = str_replace("x", $value, $formula);
+        $calculate = eval('return ' . $substitute . ';');
 
-    	for($i = 0 ; $i < strlen($formula) ; $i++)
-    	{
-    		if($formula[$i] != ')')
-    		{
-    			array_push($tmp, $formula[$i]);
-    		}
-
-    		if($i == (strlen($formula) - 1) || $formula[$i] == ')')
-    		{
-    			$num = "";
-    			$cal = 0;
-    			$sym = "";
-
-    			for($j = (count($tmp) - 1) ; $j >= 0 ; $j--)
-    			{	
-    				$pop = array_pop($tmp);
-
-					if($pop == '(' || $j == 0)
-					{
-						if($pop != '(')
-						{
-							$num = $num . $pop;
-						}
-
-						if($num == 'x')
-						{
-							$convert = $this->calculate($sym, (float)$value, $cal);
-						}
-						else if(in_array($pop, $symbols))
-						{
-							if($num == 'x')
-							{
-								$cal = (float)$value;
-							}
-							else if($num == "")
-							{
-								$cal = $convert;
-							}
-							else
-							{
-								$cal = (float)strrev($num);
-							}
-							
-							$num = "";
-							$sym = $pop;
-
-							$convert = $this->calculate($sym, $convert, $cal);
-						}
-						else
-						{
-							$convert = $this->calculate($sym, (float)strrev($num), $cal);
-						}
-
-						break;
-					}
-					else if(in_array($pop, $symbols))
-					{
-						if($num == 'x')
-						{
-							$cal = (float)$value;
-						}
-						else if($num == "")
-						{
-							$cal = $convert;
-						}
-						else
-						{
-							$cal = (float)strrev($num);
-						}
-						
-						$num = "";
-						$sym = $pop;
-						continue;
-					}
-
-					$num = $num . $pop;
-    			}
-    		}
-    	}
-
-	   	return $convert;
-    }
-
-    public function calculate($symbol, $valA, $valB)
-    {
-    	if($symbol == '+')
-    	{
-    		$cal = $valA + $valB;
-    	}
-    	else if($symbol == '-')
-    	{
-    		$cal = $valA - $valB;
-    	}
-    	else if($symbol == '*')
-    	{
-    		$cal = $valA * $valB;
-    	}
-    	else if($symbol == '/')
-    	{
-    		$cal = $valA / $valB;
-    	}
-    	else if($symbol == '^')
-    	{
-    		$cal = $valA ^ $valB;
-    	}
-
-    	return $cal;
+        return $calculate;
     }
 }
